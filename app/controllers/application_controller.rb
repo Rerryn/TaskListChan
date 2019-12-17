2134fd6e01716033b0a48774a9bfc8be002ae6b4
rerryn <5745443+Rerryn@users.noreply.github.com>
2019-12-17 23:03:59 +0000

    Still needs work, but ground work for #4 is done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
+    before_action :return_unavailable_status
     before_action :authenticate_user!
+
+
+    private
+    def return_unavailable_status
+        # This variable needs turning into some kind of check.
+        @internet = true
+        if ! @internet
+            # https://stackoverflow.com/a/9130264
+            render(:file => File.join(Rails.root, 'public/502.html'), :status => 502, :layout => false)
+        end
+        # render status: 422, :layout => nil
+    end
+
 end
